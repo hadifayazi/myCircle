b6b4362e0ab1a7e0f9b85cac183b0fa23da9b5cc
hadifayazi <hadifayazi4@gmail.com>
2024-02-25 20:54:09 +0100

    setting up store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./featurs/auth/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "login",
+    element: <Login />,
+  },
+]);
+
 function App() {
-  return <>APP</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
