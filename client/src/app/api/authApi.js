cd611dc9287aab403cf4853b0ec1d1190913ab6c
hadifayazi <hadifayazi4@gmail.com>
2024-02-26 02:08:31 +0100

    auth api for login

--- client/src/app/api/authApi.js
+++ client/src/app/api/authApi.js
@@ -1,3 +1,17 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const authApi = createApi({});
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api/v1/users/" }),
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (userData) => ({
+        url: "login",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = authApi;
