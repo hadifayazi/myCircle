8f496e23286cd60757a8cacf33c6e6a4c2ea20a4
hadifayazi <hadifayazi4@gmail.com>
2024-02-27 22:45:49 +0100

    udating uthSlice adding more details,and user details

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -3,17 +3,34 @@ import { createSlice } from "@reduxjs/toolkit";
 export const authSlice = createSlice({
   name: "authSlice",
   initialState: {
-    user: null,
+    user: {
+      id: null,
+      username: "",
+      avatar: "",
+    },
     accessToken: null,
     refreshToken: null,
+    loginError: {
+      message: "",
+      status: null,
+    },
   },
   reducers: {
     setUser: (state, action) => {
       state.user = action.payload;
     },
     setToken: (state, action) => {
-      state.accessToken = action.payload;
-      state.refreshToken = action.payload;
+      state.accessToken = action.payload.access;
+      state.refreshToken = action.payload.refresh;
+    },
+    setLoginError: (state, action) => {
+      state.loginError = {
+        message: action.payload.message,
+        status: action.payload.status,
+      };
+    },
+    clearLoginError: (state) => {
+      state.loginError = null;
     },
     logout: (state) => {
       state.user = null;
@@ -23,7 +40,9 @@ export const authSlice = createSlice({
   },
 });
 
-export const { setUser, setToken, logout } = authSlice.actions;
+export const { setUser, setToken, logout, setLoginError, clearLoginError } =
+  authSlice.actions;
 export const userSelector = (state) => state.auth.user;
 export const accessTokenSelector = (state) => state.auth.accessToken;
 export const refreshTokenSelector = (state) => state.auth.refreshToken;
+export const loginErrorSelector = (state) => state.auth.loginError;
