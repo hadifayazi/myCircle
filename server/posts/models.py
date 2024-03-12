0a795020b8b375b38a446de6c4ea8d3fd3e2c6b4
hadifayazi <hadifayazi4@gmail.com>
2024-03-12 21:46:45 +0100

    creating model for comments

--- server/posts/models.py
+++ server/posts/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from users.models import User
 
 
-class Posts(models.Model):
+class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=255)
     image = models.ImageField(blank=True, null=True)
@@ -15,4 +15,17 @@ class Posts(models.Model):
         return self.content
 
     class Meta:
-        ordering = ('-created_at',)
+        ordering = _('created_at',)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        ordering = _('created_at',)
