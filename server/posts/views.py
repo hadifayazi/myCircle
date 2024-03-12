13d18ba436fbcef1c0e422fe4675b0d4c960df43
hadifayazi <hadifayazi4@gmail.com>
2024-03-12 22:41:19 +0100

    creating view to create and list posts

--- server/posts/views.py
+++ server/posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Post, Comment
+from .serializers import CommentSerializer, MyPostSerializer, PostSerializer
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from backend.pagination import CustomPagination
 
-# Create your views here.
+
+class PostView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = CustomPagination
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
