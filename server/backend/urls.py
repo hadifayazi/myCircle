314c5a9d249f37a796ffecd3ad4cc6c34ecb9122
hadifayazi <hadifayazi4@gmail.com>
2024-03-12 21:29:47 +0100

    creating posts app

--- server/backend/urls.py
+++ server/backend/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/users/', include('users.urls')),
+    path('api/v1/posts/', include('posts.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
