1a63def07581c8a87986837a55b1258213354abd
hadifayazi <hadifayazi4@gmail.com>
2024-03-21 01:58:24 +0100

    modifing decode to return more than username

--- client/src/app/services/tokenDecode.js
+++ client/src/app/services/tokenDecode.js
@@ -1,6 +1,11 @@
 import { jwtDecode } from "jwt-decode";
 
-export const getLocalUsername = (token) => {
+export const getLocalUserInfo = (token) => {
   const decoded = jwtDecode(token);
-  return decoded.username;
+  const baseURL = "http://127.0.0.1:8000";
+  return {
+    username: decoded.username,
+    userId: decoded.user_id,
+    avatar: `${baseURL}${decoded.avatar}`,
+  };
 };
