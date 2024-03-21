1fe68cc0375ba4aaa42ea31ffca5dcb430aaa57f
hadifayazi <hadifayazi4@gmail.com>
2024-03-21 01:55:51 +0100

    pagination modified

--- server/backend/pagination.py
+++ server/backend/pagination.py
@@ -4,24 +4,24 @@ from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
     page_size = 10
-    max_page_size = 10
+    max_page_size = 200
     page_size_query_param = 'page_size'
     page_query_param = 'page'
 
     def get_paginated_response(self, data):
         return Response({
-            'count': self.page.paginator.count,
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
             'results': data,
+            "meta": {'count': self.page.paginator.count,
+                     'next': self.get_next_link(),
+                     'previous': self.get_previous_link()},
         })
 
-    def get_next_link(self):
-        if self.page.has_next():
-            return self.page.next_page_number
-        return None
+    # def get_next_link(self):
+    #     if self.page.has_next():
+    #         return self.page.next_page_number
+    #     return None
 
-    def get_previous_link(self):
-        if self.page.has_previous():
-            return self.page.previous_page_number
-        return None
+    # def get_previous_link(self):
+    #     if self.page.has_previous():
+    #         return self.page.previous_page_number
+    #     return None
