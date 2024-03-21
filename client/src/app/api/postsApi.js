e0c63fafe2d842c6ea557782b780e760f0215439
hadifayazi <hadifayazi4@gmail.com>
2024-03-21 01:57:08 +0100

    creating postsApi

--- client/src/app/api/postsApi.js
+++ client/src/app/api/postsApi.js
@@ -9,10 +9,37 @@ export const postsApi = createApi({
     baseQueryReauthorized(args, api, extraOptions, baseurl),
   endpoints: (builder) => ({
     getPosts: builder.query({
-      query: () => "",
-      providesTags: ["posts"],
+      query: (pageParam) => {
+        return {
+          url: pageParam ? `?page=${pageParam}` : "",
+        };
+      },
+
+      transformResponse: (response) => {
+        const modifiedResults = response.results.map((post) => ({
+          ...post,
+          avatar: modifyAvatarURL(post.avatar),
+        }));
+        return { ...response, results: modifiedResults };
+      },
+      providesTags: ["Post"],
+    }),
+    addPost: builder.mutation({
+      query: (post) => {
+        return {
+          url: "",
+          method: "POST",
+          body: post,
+        };
+      },
+      invalidatesTags: ["Post"],
     }),
   }),
 });
 
-export const { useGetPostsQuery } = postsApi;
+const modifyAvatarURL = (avatar) => {
+  const hostUrl = "http://127.0.0.1:8000";
+  return `${hostUrl}${avatar}`;
+};
+
+export const { useGetPostsQuery, useAddPostMutation } = postsApi;
